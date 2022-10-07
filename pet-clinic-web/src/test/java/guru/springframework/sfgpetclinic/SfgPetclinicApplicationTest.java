4e20cded68bec9d24ac0251fca3da455b97779d2
SeTThink <darkbluee2323@gmail.com>
2022-10-07 13:59:07 +0300

    Fixed Tests

---
+++ pet-clinic-web/src/test/java/guru/springframework/sfgpetclinic/SfgPetclinicApplicationTest.java
@@ -0,0 +1,12 @@
+package guru.springframework.sfgpetclinic;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SfgPetclinicApplicationTest {
+
+    @Test
+    void main() {
+    }
+}
